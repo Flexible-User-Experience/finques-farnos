0778f400b69bd75bd77c516dc5b79708451c1eac
David Romaní <david@flux.cat>
2014-11-09 21:10:56 +0100

    add empty category repository

---
+++ src/FinquesFarnos/AppBundle/Repository/CategoryRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FinquesFarnos\AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * CategoryRepository class
+ *
+ * @category Repository
+ * @package  FinquesFarnos\AppBundle\Repository
+ * @author   David Romaní <david@flux.cat>
+ */
+class CategoryRepository extends EntityRepository
+{
+}
