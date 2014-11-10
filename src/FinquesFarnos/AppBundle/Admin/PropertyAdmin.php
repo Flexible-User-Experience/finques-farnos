35c42291ef08d7b7c5e0a9348313bea2a338c6fe
David Romaní <david@flux.cat>
2014-11-10 18:50:58 +0100

    add property admin

---
+++ src/FinquesFarnos/AppBundle/Admin/PropertyAdmin.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace FinquesFarnos\AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+/**
+ * PropertyAdmin class
+ *
+ * @category Admin
+ * @package  FinquesFarnos\AppBundle\Admin
+ * @author   David Romaní <david@flux.cat>
+ */
+class PropertyAdmin extends BaseAdmin
+{
+    /**
+     * Base admin route pattern
+     *
+     * @var string
+     */
+    protected $baseRoutePattern = 'property';
+
+    /**
+     * Form view
+     *
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('Tipus', array('class' => 'col-md-6'))
+            ->add('name', 'text', array('label' => 'Nom'))
+            ->end()
+            ->with('Controls', array('class' => 'col-md-6'))
+            ->add('enabled', 'checkbox', array('label' => 'Actiu', 'required' => false))
+            ->end();
+    }
+
+    /**
+     * List view
+     *
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('name', null, array('label' => 'Tipus', 'editable' => true))
+            ->add('enabled', null, array('label' => 'Actiu', 'editable' => true))
+            ->add('_action', 'actions', array(
+                    'actions' => array(
+                        'edit' => array(),
+                    ),
+                    'label' => 'Accions',
+                ));
+    }
+}
