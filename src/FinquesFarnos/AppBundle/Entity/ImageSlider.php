2e0209eff247ad054a0388884614e80ca0fe0d92
David Romaní <david@flux.cat>
2014-11-12 17:48:08 +0100

    add image slider entity

---
+++ src/FinquesFarnos/AppBundle/Entity/ImageSlider.php
@@ -0,0 +1,301 @@
+<?php
+
+namespace FinquesFarnos\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * ImageSlider class
+ *
+ * @category Entity
+ * @package  FinquesFarnos\AppBundle\Entity
+ * @author   David Romaní <david@flux.cat>
+ *
+ * @ORM\Entity(repositoryClass="FinquesFarnos\AppBundle\Repository\ImageSliderRepository")
+ * @ORM\Table(name="image_slider")
+ * @Gedmo\TranslationEntity(class="FinquesFarnos\AppBundle\Entity\Translations\ImageSliderTranslation")
+ * @Vich\Uploadable
+ */
+class ImageSlider extends Base
+{
+    /**
+     * @Vich\UploadableField(mapping="slider_image", fileNameProperty="imageName")
+     * @Assert\File(
+     *     maxSize = "10M",
+     *     mimeTypes = {"image/jpg", "image/jpeg", "image/png", "image/gif"},
+     * )
+     * @Assert\Image(minWidth = 1200)
+     * @var File $imageFile
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255, name="image_name")
+     * @var string $imageName
+     */
+    private $imageName;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\Url()
+     * @var string
+     */
+    protected $link;
+
+    /**
+     * @ORM\Column(type="string", length=255, name="meta_title", nullable=true)
+     * @Gedmo\Translatable
+     * @var string
+     */
+    private $metaTitle;
+
+    /**
+     * @ORM\Column(type="string", length=255, name="meta_alt", nullable=true)
+     * @Gedmo\Translatable
+     * @var string
+     */
+    private $metaAlt;
+
+    /**
+     * @ORM\Column(name="position", type="integer", nullable=false)
+     * @var integer
+     */
+    private $position = 1;
+
+	/**
+     * @ORM\OneToMany(
+     *     targetEntity="FinquesFarnos\AppBundle\Entity\Translations\ImageSliderTranslation",
+     *     mappedBy="object",
+     *     cascade={"persist", "remove"}
+     * )
+     * @Assert\Valid(deep = true)
+     * @var ArrayCollection
+     */
+    private $translations;
+
+	/**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->translations = new ArrayCollection();
+    }
+
+	/**
+     * To String
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->imageName ? $this->imageName : '---';
+    }
+
+    /**
+     * Set Image file
+     *
+     * @param File|UploadedFile $image
+     *
+     * @return $this
+     */
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+        if ($image) {
+            $this->updatedAt = new \DateTime('now');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get image file
+     *
+     * @return File
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * Set image name
+     *
+     * @param $imageName
+     *
+     * @return $this
+     */
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    /**
+     * Get image name
+     *
+     * @return string
+     */
+    public function getImageName()
+    {
+        return $this->imageName;
+    }
+
+    /**
+     * Set link
+     *
+     * @param string $link
+     *
+     * @return $this
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    /**
+     * Get link
+     *
+     * @return string
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * Set Position
+     *
+     * @param int $position position
+     *
+     * @return $this
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    /**
+     * Get Position
+     *
+     * @return int
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * Set meta alt
+     *
+     * @param string $metaAlt
+     *
+     * @return $this
+     */
+    public function setMetaAlt($metaAlt)
+    {
+        $this->metaAlt = $metaAlt;
+
+        return $this;
+    }
+
+    /**
+     * Get meta alt
+     *
+     * @return string
+     */
+    public function getMetaAlt()
+    {
+        return $this->metaAlt;
+    }
+
+    /**
+     * Set meta title
+     *
+     * @param string $metaTitle
+     *
+     * @return $this
+     */
+    public function setMetaTitle($metaTitle)
+    {
+        $this->metaTitle = $metaTitle;
+
+        return $this;
+    }
+
+    /**
+     * Get meta title
+     *
+     * @return string
+     */
+    public function getMetaTitle()
+    {
+        return $this->metaTitle;
+    }
+
+    /**
+     * Add translation
+     *
+     * @param Translations\ImageSliderTranslation $translation
+     *
+     * @return $this
+     */
+    public function addTranslation(Translations\ImageSliderTranslation $translation)
+    {
+        if ($translation->getContent()) {
+            $translation->setObject($this);
+            $this->translations[] = $translation;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Remove translation
+     *
+     * @param Translations\ImageSliderTranslation $translation
+     *
+     * @return $this
+     */
+    public function removeTranslation(Translations\ImageSliderTranslation $translation)
+    {
+        $this->translations->removeElement($translation);
+
+        return $this;
+    }
+
+    /**
+     * Set translations
+     *
+     * @param ArrayCollection $translations
+     *
+     * @return $this
+     */
+    public function setTranslations($translations)
+    {
+        $this->translations = $translations;
+
+        return $this;
+    }
+
+    /**
+     * Get translations
+     *
+     * @return ArrayCollection
+     */
+    public function getTranslations()
+    {
+        return $this->translations;
+    }
+}
---
+++ src/FinquesFarnos/AppBundle/Entity/Translations/ImageSliderTranslation.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace FinquesFarnos\AppBundle\Entity\Translations;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
+
+/**
+ * ImageSliderTranslation class
+ *
+ * @category Translation
+ * @package  FinquesFarnos\AppBundle\Entity\Translations
+ * @author   David Romaní <david@flux.cat>
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="image_slider_translation",
+ *   uniqueConstraints={@ORM\UniqueConstraint(name="lookup_image_slider_unique_idx", columns={
+ *     "locale", "object_id", "field"
+ *   })}
+ * )
+ */
+class ImageSliderTranslation extends AbstractPersonalTranslation
+{
+    /**
+     * @ORM\ManyToOne(targetEntity="FinquesFarnos\AppBundle\Entity\ImageSlider", inversedBy="translations")
+     * @ORM\JoinColumn(name="object_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    protected $object;
+}
