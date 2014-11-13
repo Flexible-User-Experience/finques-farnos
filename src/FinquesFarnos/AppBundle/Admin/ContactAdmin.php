d9870026f084909738564061498a1320b55fcc88
David Romaní <david@flux.cat>
2014-11-13 17:40:32 +0100

    improve contact message admin

--- src/FinquesFarnos/AppBundle/Admin/ContactAdmin.php
+++ src/FinquesFarnos/AppBundle/Admin/ContactAdmin.php
@@ -3,6 +3,7 @@
 namespace FinquesFarnos\AppBundle\Admin;
 
 use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 
@@ -36,7 +37,7 @@ class ContactAdmin extends BaseAdmin
             ->add('email', null, array('label' => 'Email'))
             ->end()
             ->with('Controls', array('class' => 'col-md-6'))
-            ->add('enabled', 'checkbox', array('label' => 'Activa', 'required' => false))
+            ->add('enabled', 'checkbox', array('label' => 'Actiu', 'required' => false))
             ->end();
     }
 
@@ -51,12 +52,27 @@ class ContactAdmin extends BaseAdmin
             ->add('name', null, array('label' => 'Nom', 'editable' => true))
             ->add('phone', null, array('label' => 'Telèfon', 'editable' => true))
             ->add('email', null, array('label' => 'Email', 'editable' => true))
-            ->add('enabled', null, array('label' => 'Activa', 'editable' => true))
+            ->add('messages', 'integer', array('label' => 'Missatges', 'template' => '::Admin/contact_messages_count_list_field.html.twig'))
+            ->add('enabled', null, array('label' => 'Actiu', 'editable' => true))
             ->add('_action', 'actions', array(
                     'actions' => array(
                         'edit' => array(),
                     ),
                     'label' => 'Accions',
                 ));
+    }
+
+    /**
+     * List filters
+     *
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name', null, array('label' => 'Nom'))
+            ->add('phone', null, array('label' => 'Telèfon'))
+            ->add('email', null, array('label' => 'Email'))
+            ->add('enabled', null, array('label' => 'Actiu'));
     }
 }
