3a947ea71e61969b6677d8e9846d8aaa27b664d5
David Romaní <david@flux.cat>
2014-12-03 10:11:46 +0100

    improve frontend properties view angular behaviour 6

--- src/FinquesFarnos/AppBundle/EventListener/DoctrineListeners.php
+++ src/FinquesFarnos/AppBundle/EventListener/DoctrineListeners.php
@@ -71,7 +71,7 @@ class DoctrineListeners
             /** @var ImageProperty $image */
             $image = $entity->getFirstEnabledImage();
             if ($image) {
-                $entity->setVirtualFirstEnabledImageUrl($this->cm->generateUrl($this->uh->asset($image, 'property_image'), '100x100'));
+                $entity->setVirtualFirstEnabledImageUrl($this->cm->generateUrl($this->uh->asset($image, 'property_image'), '300xY'));
             }
 
         }
