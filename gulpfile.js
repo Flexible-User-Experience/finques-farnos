6faf2555051ef1ec6a37b28ba86fb0cc13ce6a61
David Romaní <david@flux.cat>
2014-12-16 18:23:33 +0100

    refactor gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-// Include gulp
-var gulp = require('gulp');
+// Gulp includes
+var gulp   = require('gulp');
+var config = require('./gulp-config.json');
 
-// Include Our Plugins
+// Plugins includes
 var jshint = require('gulp-jshint');
 var less = require('gulp-less');
 var concat = require('gulp-concat');
@@ -9,46 +10,59 @@ var minifycss = require('gulp-minify-css');
 var browserSync = require('browser-sync');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var gutil = require('gulp-util');
 
-// Proxy Browser Sync
+// BROWSER: Proxy sync
 gulp.task('browser-sync', function() {
     browserSync({
-        proxy: "finquesfarnos.dev/app_dev.php"
+        proxy: config.url
     });
 });
 
-// Reload all Browsers
+// BROWSER: Reload
 gulp.task('bs-reload', function() {
     browserSync.reload();
 });
 
-// Lint Task
+// FONTS: Copy fonts dir
+gulp.task('fonts', function() {
+    return gulp.src(['bower_components/bootstrap/fonts/*', 'bower_components/font-awesome/fonts/*'])
+        .pipe(gulp.dest('web/fonts'));
+});
+
+// CSS: Compile & minify Less
+gulp.task('less', function() {
+    return gulp.src(['app/Resources/public/frontend/css/**/*.less'])
+        .pipe(concat('main.css'))
+        .pipe(less({ sourceMap: true,  paths: ['./bower_components']})).on('error', gutil.log)
+        .pipe(minifycss())
+        .pipe(gulp.dest('web/css'));
+});
+
+// JS: Lint
 gulp.task('lint', function() {
     return gulp.src('app/Resources/public/js/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
-// Compile & Minify Less in production environment
-gulp.task('less', function() {
-    return gulp.src(['bower_components/bootstrap/less/bootstrap.less', 'app/Resources/public/css/**/*.less'])
-        .pipe(concat('main.css'))
-        .pipe(less())
-        .pipe(minifycss())
-        .pipe(gulp.dest('web/css'));
-});
-
-// Compile & Minify Less in development environment
-gulp.task('devless', function() {
-    return gulp.src(['bower_components/bootstrap/less/bootstrap.less', 'app/Resources/public/css/**/*.less'])
-        .pipe(concat('main.css'))
-        .pipe(less())
-        .pipe(gulp.dest('web/css'));
-});
-
-// Concatenate & Minify JS in production environment
+// JS: Concatenate & minify vendor scripts
 gulp.task('scripts', function() {
-    return gulp.src(['bower_components/jquery/dist/jquery.js', 'bower_components/lodash/dist/lodash.js', 'bower_components/numeral/numeral.js', 'bower_components/numeral/languages/es.js', 'bower_components/modernizr/modernizr.js', 'bower_components/bootstrap/dist/js/bootstrap.js', 'bower_components/angular/angular.js', 'bower_components/angular-resource/angular-resource.js', 'bower_components/angular-cookies/angular-cookies.js', 'bower_components/angular-sanitize/angular-sanitize.js', 'bower_components/angular-route/angular-route.js', 'bower_components/angular-touch/angular-touch.js', 'bower_components/angular-google-maps/dist/angular-google-maps.js'])
+    return gulp.src([
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/lodash/dist/lodash.js',
+            'bower_components/numeral/numeral.js',
+            'bower_components/numeral/languages/es.js',
+            'bower_components/modernizr/modernizr.js',
+            'bower_components/holderjs/holder.js',
+            'bower_components/bootstrap/dist/js/bootstrap.js',
+            'bower_components/angular/angular.js',
+            'bower_components/angular-resource/angular-resource.js',
+            'bower_components/angular-cookies/angular-cookies.js',
+            'bower_components/angular-sanitize/angular-sanitize.js',
+            'bower_components/angular-route/angular-route.js',
+            'bower_components/angular-touch/angular-touch.js',
+            'bower_components/angular-google-maps/dist/angular-google-maps.js'])
         .pipe(concat('main.js'))
         .pipe(gulp.dest('web/js'))
         .pipe(rename('main.min.js'))
@@ -56,27 +70,20 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('web/js'));
 });
 
-// Concatenate & Minify JS in development environment
-gulp.task('devjs', function() {
-    return gulp.src(['bower_components/jquery/dist/jquery.js', 'bower_components/lodash/dist/lodash.js', 'bower_components/numeral/numeral.js', 'bower_components/numeral/languages/es.js', 'bower_components/modernizr/modernizr.js', 'bower_components/bootstrap/dist/js/bootstrap.js', 'bower_components/angular/angular.js', 'bower_components/angular-resource/angular-resource.js', 'bower_components/angular-cookies/angular-cookies.js', 'bower_components/angular-sanitize/angular-sanitize.js', 'bower_components/angular-route/angular-route.js', 'bower_components/angular-touch/angular-touch.js', 'bower_components/angular-google-maps/dist/angular-google-maps.js'])
-        .pipe(gulp.dest('web/js'));
-});
-
-// Concatenate & Minify JS in production environment
+// JS: Concatenate & minify custom scripts
 gulp.task('myjs', function() {
     return gulp.src('app/Resources/public/js/**/*.js')
         .pipe(concat('my.js'))
         .pipe(gulp.dest('web/js'))
-        .pipe(rename('my.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('web/js'));
+        .pipe(rename('my.min.js'));
 });
 
-// Watch Files For Changes
+// Watch
 gulp.task('watch', ['browser-sync'], function() {
+    gulp.watch('app/Resources/views/Front/**/*.twig', ['bs-reload']);
     gulp.watch('app/Resources/public/js/**/*.js', ['lint', 'myjs', 'bs-reload']);
     gulp.watch('app/Resources/public/css/**/*.less', ['less', 'bs-reload']);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'less', 'scripts', 'myjs']);
+gulp.task('default', ['lint', 'fonts', 'less', 'scripts', 'myjs']);
