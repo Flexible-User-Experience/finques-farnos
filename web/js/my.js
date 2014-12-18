a1536110c1ba080868c0170de5b7931e8f4d42f7
David Romaní <david@flux.cat>
2014-12-18 11:00:58 +0100

    cup

--- web/js/my.js
+++ web/js/my.js
@@ -52,16 +52,13 @@ angular.module('propertiesApp')
 angular.module('propertiesApp')
     .controller('PropertyDetailCtrl', ['CFG', 'uiGmapGoogleMapApi', '$scope', '$log', function (CFG, uiGmapGoogleMapApi, $scope, $log) {
 
-        $scope.init = function(propertiesFormFilter, filteredProperties) {
-            $scope.firstCallFinished = true;
-            $scope.type = {};
+        $scope.init = function(localization) {
             $scope.map = { center: { latitude: 41, longitude: 0 }, zoom: 4, bounds: {}, clusterOptions: { gridSize: 80, maxZoom: 20, averageCenter: true, minimumClusterSize: 1, zoomOnClick: false } };
             $scope.map.options = { scrollwheel: true, draggable: true, maxZoom: 15 };
             $scope.map.control = {};
 
-            $scope.form = angular.fromJson(propertiesFormFilter);
-            $scope.properties = angular.fromJson(filteredProperties);
-
+            $scope.localization = angular.fromJson(localization);
+            $log.log($scope.localization);
         };
 
         uiGmapGoogleMapApi.then(function(maps) {
