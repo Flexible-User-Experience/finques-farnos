6e6c8a37456194f2cef2531301c860308194eb2b
David Romaní <david@flux.cat>
2015-01-07 09:20:54 +0100

    improve property admin edit view

--- src/FinquesFarnos/AppBundle/Admin/ImagePropertyAdmin.php
+++ src/FinquesFarnos/AppBundle/Admin/ImagePropertyAdmin.php
@@ -47,17 +47,17 @@ class ImagePropertyAdmin extends BaseAdmin
             ->add('metaTitle', null, array('label' => 'Títol (SEO)'))
             ->add('metaAlt', null, array('label' => 'Alt (SEO)'))
             ->end()
-            ->with('Traduccions', array('class' => 'col-md-6'))
-            ->add('translations', 'a2lix_translations_gedmo', array(
-                    'required' => false,
-                    'label' => ' ',
-                    'translatable_class' => 'FinquesFarnos\AppBundle\Entity\Translation\ImageTranslation',
-                    'fields' => array(
-                        'metaTitle' => array('label' => 'Títol (SEO)', 'required' => false),
-                        'metaAlt' => array('label' => 'Alt (SEO)', 'required' => false),
-                    ),
-                ))
-            ->end()
+//            ->with('Traduccions', array('class' => 'col-md-6'))
+//            ->add('translations', 'a2lix_translations_gedmo', array(
+//                    'required' => false,
+//                    'label' => ' ',
+//                    'translatable_class' => 'FinquesFarnos\AppBundle\Entity\Translation\ImageTranslation',
+//                    'fields' => array(
+//                        'metaTitle' => array('label' => 'Títol (SEO)', 'required' => false),
+//                        'metaAlt' => array('label' => 'Alt (SEO)', 'required' => false),
+//                    ),
+//                ))
+//            ->end()
             ->with('Controls', array('class' => 'col-md-6'))
             ->add('position', null, array('label' => 'Posició', 'required' => true))
             ->add('enabled', 'checkbox', array('label' => 'Activa', 'required' => false))
@@ -129,6 +129,6 @@ class ImagePropertyAdmin extends BaseAdmin
         /** @var UploaderHelper $vus */
         $vus = $this->getConfigurationPool()->getContainer()->get('vich_uploader.templating.helper.uploader_helper');
 
-        return ($this->getSubject() ? $this->getSubject()->getImageName() ? '<img src="'.$lis->getBrowserPath($vus->asset($this->getSubject(), 'imageFile'), '300xY').'" class="admin-preview" alt=""/>' : '' : '').'<span style="width:100%;display:block;">Màxim 10MB amb format PNG, JPG o GIF. Imatge amb amplada mínima de 1.200px.</span>';
+        return ($this->getSubject() ? $this->getSubject()->getImageName() ? '<img src="'.$lis->getBrowserPath($vus->asset($this->getSubject(), 'imageFile'), '100x60').'" class="admin-preview" alt=""/>' : '' : '').'<span style="width:100%;display:block;">Màxim 10MB amb format PNG, JPG o GIF. Imatge amb amplada mínima de 1.200px.</span>';
     }
 }
