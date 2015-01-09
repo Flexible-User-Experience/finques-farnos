ab980ddc51da44df3f574afa84ad1e08c72ce86d
David Romaní <david@flux.cat>
2015-01-09 10:43:21 +0100

    improve web pdf generator 1

--- src/FinquesFarnos/AppBundle/PdfGenerator/CustomTcpdf.php
+++ src/FinquesFarnos/AppBundle/PdfGenerator/CustomTcpdf.php
@@ -28,11 +28,36 @@ class CustomTcpdf extends \TCPDF
      */
     public function Footer()
     {
-        // Position at 15 mm from bottom
-        $this->SetY(-15);
-        // Set font
-        $this->SetFont('helvetica', 'I', 8);
-        // Page number
-        $this->Cell(0, 10, 'Page '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+        $this->SetY(-20);
+        $this->SetFont('helvetica', '', 9);
+        $this->setGreyColor();
+        $this->drawBrandLine($this->GetY());
+        $this->SetX($this->getMargins()['left'] - 2);
+        $this->Cell(0, 15, 'C. Corsini 61 · 43870 Amposta · Tarragona · 977 702 721 · info@finquesfarnos.com · www.finquesfarnos.com', 0, false, 'L', 0, '', 0, false, 'T', 'C');
+    }
+
+    public function setOrangeColor()
+    {
+        $this->SetTextColor(216, 111, 36);
+    }
+
+    public function setGreyColor()
+    {
+        $this->SetTextColor(101, 91, 69);
+    }
+
+    public function brandLineStyle()
+    {
+        return array(
+            'width' => 2,
+            'cap' => 'square',
+            'join' => 'miter',
+            'color' => array(216, 111, 36),
+        );
+    }
+
+    public function drawBrandLine($y)
+    {
+        $this->Line($this->getMargins()['left'], $y, $this->getMargins()['right'], $y, $this->brandLineStyle());
     }
 }
