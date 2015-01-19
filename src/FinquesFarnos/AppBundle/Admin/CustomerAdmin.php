2702aec84186cedefe8f54b19556bfb8661ff614
David Romaní <david@flux.cat>
2015-01-19 17:23:06 +0100

    improve customer admin

--- src/FinquesFarnos/AppBundle/Admin/CustomerAdmin.php
+++ src/FinquesFarnos/AppBundle/Admin/CustomerAdmin.php
@@ -32,12 +32,14 @@ class CustomerAdmin extends BaseAdmin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->with('Contacte', array('class' => 'col-md-6'))
+            ->with('Client', array('class' => 'col-md-6'))
             ->add('dni', null, array('label' => 'DNI'))
             ->add('name', null, array('label' => 'Nom'))
             ->add('phone', null, array('label' => 'Telèfon'))
             ->add('mobile', null, array('label' => 'Mòbil'))
             ->add('email', null, array('label' => 'Email'))
+            ->end()
+            ->with('Adreça', array('class' => 'col-md-6'))
             ->add('address', null, array('label' => 'Adreça'))
             ->add('city', null, array('label' => 'Població'))
             ->add('postalCode', null, array('label' => 'Codi postal'))
@@ -60,7 +62,7 @@ class CustomerAdmin extends BaseAdmin
             ->add('name', null, array('label' => 'Nom', 'editable' => true))
             ->add('phone', null, array('label' => 'Telèfon', 'editable' => true))
             ->add('mobile', null, array('label' => 'Mòbil', 'editable' => true))
-            ->add('email', null, array('label' => 'Email', 'editable' => false))
+            ->add('email', null, array('label' => 'Email', 'editable' => true))
             ->add('enabled', null, array('label' => 'Actiu', 'editable' => true))
             ->add('_action', 'actions', array(
                     'actions' => array(
@@ -78,8 +80,8 @@ class CustomerAdmin extends BaseAdmin
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
+            ->add('dni', null, array('label' => 'Dni'))
             ->add('name', null, array('label' => 'Nom'))
-            ->add('dni', null, array('label' => 'Nom'))
             ->add('email', null, array('label' => 'Email'))
             ->add('enabled', null, array('label' => 'Actiu'));
     }
