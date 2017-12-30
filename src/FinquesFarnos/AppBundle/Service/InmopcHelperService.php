e4780e2b6221cf2fa00f5a91c27a8af0e976ac83
David Romaní <david@flux.cat>
2017-12-30 12:34:43 +0100

    implement inmopc helper service builder 2

--- src/FinquesFarnos/AppBundle/Service/InmopcHelperService.php
+++ src/FinquesFarnos/AppBundle/Service/InmopcHelperService.php
@@ -13,6 +13,28 @@ class InmopcHelperService
 {
     const INMOPC_BASE_URL = 'https://www.forocasas.com/recursos/nlistadom1.php?';
 
+    const INMOPC_LOCALE_ES = 1;
+    const INMOPC_LOCALE_EN = 2;
+    const INMOPC_LOCALE_DE = 3;
+    const INMOPC_LOCALE_FR = 4;
+    const INMOPC_LOCALE_CA = 5;
+    const INMOPC_LOCALE_RU = 8;
+
+    const INMOPC_TYPE_HOUSE = 'G1';
+    const INMOPC_TYPE_OFFICE = 'G3';
+    const INMOPC_TYPE_SHIP = 'G4';
+    const INMOPC_TYPE_GARAGE = 'G5';
+    const INMOPC_TYPE_SOLAR = 'G6';
+    const INMOPC_TYPE_COUNTRY = 'G8';
+
+    const INMOPC_COLOR_ORANGE = 'D86F24';
+    const INMOPC_COLOR_ORANGE_LIGHT = 'F9F0E8';
+    const INMOPC_COLOR_ORANGE_DARK = 'E6C3A6';
+    const INMOPC_COLOR_GREY_LOGO = '655B45';
+    const INMOPC_COLOR_GREY = '424242';
+    const INMOPC_COLOR_WHITE = 'FFFFFF';
+    const INMOPC_COLOR_BLACK = '000000';
+
     /**
      * @var string
      */
@@ -35,7 +57,7 @@ class InmopcHelperService
     public function __construct($inmopcCustomerCode)
     {
         $this->inmopcCustomerCode = $inmopcCustomerCode;
-        $this->inmopcIframeSrcUrl = self::INMOPC_BASE_URL.'cod_cliente='.$inmopcCustomerCode.'&cod_captacion='.$inmopcCustomerCode.'&est_bus=1';
+        $this->inmopcIframeSrcUrl = self::INMOPC_BASE_URL.'cod_cliente='.$inmopcCustomerCode.'&cod_captacion='.$inmopcCustomerCode.'&est_bus=1&colorfondo='.self::INMOPC_COLOR_ORANGE_LIGHT.'&colorfondo2='.self::INMOPC_COLOR_ORANGE.'&colortitulo='.self::INMOPC_COLOR_BLACK.'&bg_buscador='.self::INMOPC_COLOR_ORANGE;
     }
 
     /**
